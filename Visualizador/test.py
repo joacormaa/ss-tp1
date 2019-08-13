d6acbec10f5087f57383604c95895a60b8f6bfe8
joaquinorma <joaquinorma@gmail.com>
2019-08-13 11:05:40 -0300

    Radius mod

--- Visualizador/test.py
+++ Visualizador/test.py
@@ -5,10 +5,10 @@ import json
 systemLength = 100
 particleRadius = 1
 particleRC = 1
-frontierRadius = 2*particleRadius + particleRC
-focusId = 0
+frontierRadius = particleRadius + particleRC
+focusId = 696
 
-with open('input2.json') as f:
+with open('input3.json') as f:
     data = json.load(f)
 
 myX = []
@@ -36,15 +36,15 @@ p = figure(
 green_box = BoxAnnotation(bottom=0, top=100, left=0, right=100, fill_color='green', fill_alpha=0.1)
 p.add_layout(green_box)
 
-p.circle(myX, myY, size=particleRadius, color="navy", alpha=0.5)
-r = p.circle([focusX],  [focusY], size=particleRadius, color="red", alpha=0.5)
+p.circle(myX, myY, radius=particleRadius, color="navy", alpha=0.5)
+r = p.circle([focusX],  [focusY], radius=particleRadius, color="black", alpha=0.5)
 glyph = r.glyph
-glyph.size = frontierRadius
+glyph.radius = frontierRadius
 glyph.fill_alpha = 0.2
 glyph.line_color = "firebrick"
 glyph.line_dash = [6, 3]
 glyph.line_width = 2
 
-p.circle(focusNeighborsX, focusNeighborsY, size=10, color="black", alpha=0.5)
+p.circle(focusNeighborsX, focusNeighborsY, radius=particleRadius, color="black", alpha=0.5)
 
 show(p)
