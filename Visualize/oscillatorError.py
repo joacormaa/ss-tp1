b9fad5bcf84f969cc95fe488e6a3d4d5a91ad44b
pabloito <pradnic@itba.edu.ar>
2019-09-24 16:23:37 -0300

    improvements

--- Visualize/oscillatorError.py
+++ Visualize/oscillatorError.py
@@ -1,66 +1,94 @@
 import pandas as pd
 import numpy as np
+from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 
-dfa = pd.read_csv('../output/ANALYTICALParticle001.csv')
-dfa.sort_values(by=['time'], inplace=True)
 
-df1a = pd.read_csv('../output/VERLETParticle001.csv')
-df1a.sort_values(by=['time'], inplace=True)
+x = [0.01, 0.001, 0.0006, 0.0001, 0.00005]
+methods = ['ANALYTICAL','VERLET','BEEMAN','GPCO5']
 
-df2a = pd.read_csv('../output/BEEMANParticle001.csv')
-df2a.sort_values(by=['time'], inplace=True)
-
-df3a = pd.read_csv('../output/GPCO5Particle001.csv')
-df3a.sort_values(by=['time'], inplace=True)
-
-dfb = pd.read_csv('../output/ANALYTICALParticle0006.csv')
-dfb.sort_values(by=['time'], inplace=True)
-
-df1b = pd.read_csv('../output/VERLETParticle0006.csv')
-df1b.sort_values(by=['time'], inplace=True)
-
-df2b = pd.read_csv('../output/BEEMANParticle0006.csv')
-df2b.sort_values(by=['time'], inplace=True)
-
-df3b = pd.read_csv('../output/GPCO5Particle0006.csv')
-df3b.sort_values(by=['time'], inplace=True)
-
-dfc = pd.read_csv('../output/ANALYTICALParticle0001.csv')
-dfc.sort_values(by=['time'], inplace=True)
-
-df1c = pd.read_csv('../output/VERLETParticle0001.csv')
-df1c.sort_values(by=['time'], inplace=True)
-
-df2c = pd.read_csv('../output/BEEMANParticle0001.csv')
-df2c.sort_values(by=['time'], inplace=True)
-
-df3c = pd.read_csv('../output/GPCO5Particle0001.csv')
-df3c.sort_values(by=['time'], inplace=True)
-
-dfd = pd.read_csv('../output/ANALYTICALParticle00005.csv')
-dfd.sort_values(by=['time'], inplace=True)
-
-df1d = pd.read_csv('../output/VERLETParticle00005.csv')
-df1d.sort_values(by=['time'], inplace=True)
-
-df2d = pd.read_csv('../output/BEEMANParticle00005.csv')
-df2d.sort_values(by=['time'], inplace=True)
-
-df3d = pd.read_csv('../output/GPCO5Particle00005.csv')
-df3d.sort_values(by=['time'], inplace=True)
+vals = [[0 for i in range(len(methods))] for j in range(len(x))]
 
 dfe = pd.read_csv('../output/ANALYTICALParticle01.csv')
 dfe.sort_values(by=['time'], inplace=True)
+vals[0][0] = dfe['position']
 
 df1e = pd.read_csv('../output/VERLETParticle01.csv')
 df1e.sort_values(by=['time'], inplace=True)
+vals[0][1] = df1e['position']
 
 df2e = pd.read_csv('../output/BEEMANParticle01.csv')
 df2e.sort_values(by=['time'], inplace=True)
+vals[0][2] = df2e['position']
 
 df3e = pd.read_csv('../output/GPCO5Particle01.csv')
 df3e.sort_values(by=['time'], inplace=True)
+vals[0][3] = df3e['position']
+
+dfa = pd.read_csv('../output/ANALYTICALParticle001.csv')
+dfa.sort_values(by=['time'], inplace=True)
+vals[1][0] = dfa['position']
+
+df1a = pd.read_csv('../output/VERLETParticle001.csv')
+df1a.sort_values(by=['time'], inplace=True)
+vals[1][1] = df1a['position']
+
+
+df2a = pd.read_csv('../output/BEEMANParticle001.csv')
+df2a.sort_values(by=['time'], inplace=True)
+vals[1][2] = df2a['position']
+
+df3a = pd.read_csv('../output/GPCO5Particle001.csv')
+df3a.sort_values(by=['time'], inplace=True)
+vals[1][3] = df3a['position']
+
+dfb = pd.read_csv('../output/ANALYTICALParticle0006.csv')
+dfb.sort_values(by=['time'], inplace=True)
+vals[2][0] = dfb['position']
+
+df1b = pd.read_csv('../output/VERLETParticle0006.csv')
+df1b.sort_values(by=['time'], inplace=True)
+vals[2][1] = df1b['position']
+
+df2b = pd.read_csv('../output/BEEMANParticle0006.csv')
+df2b.sort_values(by=['time'], inplace=True)
+vals[2][2] = df2b['position']
+
+df3b = pd.read_csv('../output/GPCO5Particle0006.csv')
+df3b.sort_values(by=['time'], inplace=True)
+vals[2][3] = df3b['position']
+
+dfc = pd.read_csv('../output/ANALYTICALParticle0001.csv')
+dfc.sort_values(by=['time'], inplace=True)
+vals[3][0] = dfc['position']
+
+df1c = pd.read_csv('../output/VERLETParticle0001.csv')
+df1c.sort_values(by=['time'], inplace=True)
+vals[3][1] = df1c['position']
+
+df2c = pd.read_csv('../output/BEEMANParticle0001.csv')
+df2c.sort_values(by=['time'], inplace=True)
+vals[3][2] = df2c['position']
+
+df3c = pd.read_csv('../output/GPCO5Particle0001.csv')
+df3c.sort_values(by=['time'], inplace=True)
+vals[3][3] = df3c['position']
+
+dfd = pd.read_csv('../output/ANALYTICALParticle00005.csv')
+dfd.sort_values(by=['time'], inplace=True)
+vals[4][0] = dfd['position']
+
+df1d = pd.read_csv('../output/VERLETParticle00005.csv')
+df1d.sort_values(by=['time'], inplace=True)
+vals[4][1] = df1d['position']
+
+df2d = pd.read_csv('../output/BEEMANParticle00005.csv')
+df2d.sort_values(by=['time'], inplace=True)
+vals[4][2] = df2d['position']
+
+df3d = pd.read_csv('../output/GPCO5Particle00005.csv')
+df3d.sort_values(by=['time'], inplace=True)
+vals[4][3] = df3d['position']
 
 '''
 time = 10
@@ -76,102 +104,43 @@ cutd = int(time/stepd)
 cute = int(time/stepe)
 '''
 
-x = [0.01, 0.001, 0.0006, 0.0001, 0.00005]
 
-yVerlet = []
-yBeeman = []
-yGPCo5 = []
-yVerletErr = []
-yBeemanErr = []
-yGPCo5Err = []
-#ANALYTICAL
-ya = dfa['position']
-yb = dfb['position']
-yc = dfc['position']
-yd = dfd['position']
-ye = dfe['position']
+for i in range(len(x)):
+    expected = vals[i][0]
+    for j in range(len(methods)):
+        vals[i][j].sub(expected)
 
-#VERLET
-y1a = df1a['position']
-y1a_e = abs(y1a.subtract(ya))
-y1b = df1b['position']
-y1b_e = abs(y1b.subtract(yb))
-y1c = df1c['position']
-y1c_e = abs(y1c.subtract(yc))
-y1d = df1d['position']
-y1d_e = abs(y1d.subtract(yd))
-y1e = df1e['position']
-y1e_e = abs(y1e.subtract(ye))
 
-#BEEMAN
-y2a = df2a['position']
-y2a_e = abs(y2a.subtract(ya))
-y2b = df2b['position']
-y2b_e = abs(y2b.subtract(yb))
-y2c = df2c['position']
-y2c_e = abs(y2c.subtract(yc))
-y2d = df2d['position']
-y2d_e = abs(y2d.subtract(yd))
-y2e = df2e['position']
-y2e_e = abs(y2e.subtract(ye))
+yval = [[0 for i in range(len(methods))] for j in range(len(x))]
+yerr = [[0 for i in range(len(methods))] for j in range(len(x))]
 
-#GPCo5
-y3a = df3a['position']
-y3a_e = abs(y3a.subtract(ya))
-y3b = df3b['position']
-y3b_e = abs(y3b.subtract(yb))
-y3c = df3c['position']
-y3c_e = abs(y3c.subtract(yc))
-y3d = df3d['position']
-y3d_e = abs(y3d.subtract(yd))
-y3e = df3e['position']
-y3e_e = abs(y3e.subtract(ye))
+yval = np.array(yval)
+yerr = np.array(yerr)
+
+yval.dtype = 'float32'
+yerr.dtype = 'float32'
 
 #Errores Normalizados
-yVerlet.append(np.mean(y1e_e))
-yVerletErr.append(y1e_e.std())
-yVerlet.append(np.mean(y1a_e))
-yVerletErr.append(y1a_e.std())
-yVerlet.append(np.mean(y1b_e))
-yVerletErr.append(y1b_e.std())
-yVerlet.append(np.mean(y1c_e))
-yVerletErr.append(y1c_e.std())
-yVerlet.append(np.mean(y1d_e))
-yVerletErr.append(y1d_e.std())
 
-yBeeman.append(np.mean(y2e_e))
-yBeemanErr.append(y2e_e.std())
-yBeeman.append(np.mean(y2a_e))
-yBeemanErr.append(y2a_e.std())
-yBeeman.append(np.mean(y2b_e))
-yBeemanErr.append(y2b_e.std())
-yBeeman.append(np.mean(y2c_e))
-yBeemanErr.append(y2c_e.std())
-yBeeman.append(np.mean(y2d_e))
-yBeemanErr.append(y2d_e.std())
+for i in range(len(x)):
+    for j in range(len(methods)):
+        curr = vals[i][j]
+        yval[i][j] = np.mean(np.power(curr, 2))
+        yerr[i][j] = np.std(curr)
 
-yGPCo5.append(np.mean(y3e_e))
-yGPCo5Err.append(y3e_e.std())
-yGPCo5.append(np.mean(y3a_e))
-yGPCo5Err.append(y3a_e.std())
-yGPCo5.append(np.mean(y3b_e))
-yGPCo5Err.append(y3b_e.std())
-yGPCo5.append(np.mean(y3c_e))
-yGPCo5Err.append(y3c_e.std())
-yGPCo5.append(np.mean(y3d_e))
-yGPCo5Err.append(y3d_e.std())
-
+print(yval)
+print(yerr)
 fig = plt.figure()
 plt.title('Oscilador')
 plt.xlabel('dt')
 plt.ylabel('Error (m)')
 
 #con error
-'''
-plt.errorbar(x, yVerlet, yerr=yVerletErr, lw=0.5, ms=0.5, marker='.', c='red', elinewidth=1, capsize=5, label='VERLET')
-plt.errorbar(x, yBeeman, yerr=yBeemanErr, lw=0.5, ms=0.5, marker='.', c='green', elinewidth=1, capsize=5, label='BEEMAN')
-plt.errorbar(x, yGPCo5, yerr=yGPCo5Err, lw=0.5, ms=0.5, marker='.', c='purple', elinewidth=1, capsize=5, label='GPCo5')
-'''
+plt.errorbar(x, yval[:,0], yerr=yerr[:,0], lw=0.5, ms=0.5, marker='.', c='red', elinewidth=1, capsize=5, label=methods[0])
+plt.errorbar(x, yval[:,1], yerr=yerr[:,1], lw=0.5, ms=0.5, marker='.', c='green', elinewidth=1, capsize=5, label=methods[1])
+plt.errorbar(x, yval[:,2], yerr=yerr[:,2], lw=0.5, ms=0.5, marker='.', c='blue', elinewidth=1, capsize=5, label=methods[2])
+plt.errorbar(x, yval[:,3], yerr=yerr[:,3], lw=0.5, ms=0.5, marker='.', c='purple', elinewidth=1, capsize=5, label=methods[3])
+
 #sin error
 '''
 plt.plot(x, yVerlet, lw=0.5, ms=0.5, marker='.', c='red', label='VERLET')
@@ -179,17 +148,11 @@ plt.plot(x, yBeeman, lw=0.5, ms=0.5, marker='.', c='green', label='BEEMAN')
 plt.plot(x, yGPCo5, lw=0.5, ms=0.5, marker='.', c='purple', label='GPCo5')
 '''
 
-plt.plot(df2c['time'], y2c_e, label='BEEMAN')
-
 plt.legend(loc='upper right')
 plt.yscale("log")
+plt.xscale("log")
 plt.xticks(x)
 plt.grid(ls='--')
-
-print(x)
-print(yVerlet)
-print(yBeeman)
-print(yGPCo5)
 
 plt.show()
 
