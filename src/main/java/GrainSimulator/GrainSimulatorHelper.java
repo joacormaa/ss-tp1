7420b246c6dcec396c7444d53cc06c862db01c6e
joaquinorma <joaquinorma@gmail.com>
2019-10-04 11:36:37 -0300

    Force Structure

--- src/main/java/GrainSimulator/GrainSimulatorHelper.java
+++ src/main/java/GrainSimulator/GrainSimulatorHelper.java
@@ -4,32 +4,54 @@ import Constants.Config;
 import Model.Particle;
 
 public class GrainSimulatorHelper {
+    public class Force{
+        double normalForce;
+        double tangentialForce;
+
+        public Force(double normalForce, double tangentialForce){
+            this.normalForce = normalForce;
+            this.tangentialForce = tangentialForce;
+        }
+    }
     Config c;
 
     public GrainSimulatorHelper() {
         c = Config.getInstance();
     }
-    public double getForce(Particle p1, Particle p2) {
+    public Force getForce(Particle p1, Particle p2) {
+        //calcular Xi
+        double xi = calculateXi(p1, p2);
+        //calculate VelRelTang
+        double velRelTang = calculateVelRelTang(p1, p2);
 
-    }
+        //Normal
+        double normalForce = getNormalForce(xi);
 
-    private double getTangencialForce(Particle p1, Particle p2) {
+        //Tangential
+        double tangentialForce = getTangentialForce(xi, velRelTang);
 
-    }
-
-    private double getNormalForce(Particle p1, Particle p2) {
-        double kn = c.KN();
-        double sigma = calculateXi(p1, p2);
-        return -kn*sigma;
+        return new Force(normalForce, tangentialForce);
     }
 
     private double calculateXi(Particle p1, Particle p2) {
         return p1.getInteractionRadius() + p2.getInteractionRadius() - getPositionDifference(p1, p2);
     }
 
+    private double calculateVelRelTang(Particle p1, Particle p2){
+        return 0;
+    }
+
     private double getPositionDifference(Particle p1, Particle p2) {
         return p1.getDistanceTo(p2);
     }
 
+    private double getNormalForce(double xi) {
+        return -c.KN()*xi;
+    }
+
+
+    private double getTangentialForce(double xi, double velRelTang) {
+        return -c.KT()*xi*velRelTang;
+    }
 
 }
