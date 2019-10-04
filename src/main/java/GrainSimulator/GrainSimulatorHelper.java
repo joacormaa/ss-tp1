c83e2d4241cfd9ef0056507eed7b719c9bfc64b7
Ariel <ari.atar1997@gmail.com>
2019-10-04 10:46:54 -0300

    grain simulator helper for orma

---
+++ src/main/java/GrainSimulator/GrainSimulatorHelper.java
@@ -0,0 +1,17 @@
+package GrainSimulator;
+
+import Model.Particle;
+
+public class GrainSimulatorHelper {
+    public double getForce(Particle p1, Particle p2) {
+
+    }
+
+    private double getTangencialForce(Particle p1, Particle p2) {
+
+    }
+
+    private double getTangencialForce(Particle p1, Particle p2) {
+
+    }
+}
